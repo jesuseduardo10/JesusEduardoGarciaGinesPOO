d598a30ee6bee357b20032177a5ddbf1636e5589
jesuseduardo10 <eduardogines10@gmail.com>
2019-01-15 16:56:45 -0600

    nuevo archivo

---
+++ HolaYo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class HolaYo{
+
+	public static void main(String[] args){
+	String nombre;
+	Scanner scanner =new Scanner(System.in);
+	System.out.println("Introduce tu nombre ");
+	nombre=scanner.next();
+	System.out.println("Hola "+nombre);
+	}
+	
+	
+
+}
---
+++ Prueba.java
@@ -0,0 +1,7 @@
+public class Prueba{
+	//atributos
+	
+	//metodos
+
+
+}
